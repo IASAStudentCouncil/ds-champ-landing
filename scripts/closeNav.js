5632f1deebda8da772fa37e860071631a1ea820e
PrometheusUA <prometheusua@i.ua>
2022-08-14 18:31:58 +0300

    Bugfix

--- scripts/closeNav.js
+++ scripts/closeNav.js
@@ -1,4 +1,5 @@
-function closeNav() {
+async function closeNav() {
     const menuButton = document.querySelector("nav input#menu-btn");
     menuButton.checked = false;
+    document.body.classList.remove("opened-nav");
 }
