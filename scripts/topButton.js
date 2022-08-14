0352a6c9acedc4dc5d864eda5a7d6502b41de13e
PrometheusUA <prometheusua@i.ua>
2022-08-14 12:35:56 +0300

    Added button to scroll to top

---
+++ scripts/topButton.js
@@ -0,0 +1,22 @@
+const button = document.querySelector("#top-button");
+
+
+window.onscroll = function() {
+    if (window.scrollY > 300) {
+        button.classList.add("show");
+    }
+    else {
+        button.classList.remove("show");
+    }
+};
+
+function toTop() {
+    // var scrollDuration = window.scrollY/8;
+    var scrollStep = -120, //window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
+}
